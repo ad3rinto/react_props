aeb58441f9c05fa700898da93cdf62144850d436
Micheal A <aad3rinto@gmail.com>
2022-10-01 17:11:39 +0000

    break into individual card components

---
+++ build/precache-manifest.6d3b8f06a041c69cf81c23f71671c964.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72874fa3d1f1badff57402cbb067e9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81480562bac5cd30bb4f",
+    "url": "/static/js/2.3fc08aa4.chunk.js"
+  },
+  {
+    "revision": "8674ec3373121c82a7ed",
+    "url": "/static/js/main.cbc2b948.chunk.js"
+  },
+  {
+    "revision": "c3872a26666b11008079",
+    "url": "/static/js/runtime-main.db02fc18.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,33 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card (props){
+  return(
+    <div>
+    <h2>{props.name}</h2>
+    <img
+      src={props.img}
+      alt="avatar_img"
+    />
+    <p>{props.tel}</p>
+    <p>{props.email}</p>
+    </div>
+
+  )
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
 
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
-    />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
+    <Card 
+      name="Beyonce"
+      img="https://www.beyonce.com/uploads/2022/09/cache/fKHYxrA6o90f_1920x1920_KaMXZQjO.jpg"
+      tel="1212122111212121"
+      email="b@be.com" 
+      />
 
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
-    />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
+      <Card 
 
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+    name="Jack Bauer"
+    img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
       alt="avatar_img"
+    
+    tel="987 654 321"
+    email="jack@nowhere.com"
+
     />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
+
+    <Card
+
+    name="Chuck Norris"
+    img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      alt="avatar_img"
+    tel="918 372 574"
+    email="gmail@chucknorris.com"
+      />
+    
   </div>,
   document.getElementById("root")
 );
